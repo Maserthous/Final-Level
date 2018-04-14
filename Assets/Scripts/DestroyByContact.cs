5cea9d3fee2b693e3caf9f74b4b6be04b8a862e3
RizwanSamji <36008740+RizwanSamji@users.noreply.github.com>
2018-04-13 23:09:06 -0400

    [ADD] Spikes

---
+++ Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByContact : MonoBehaviour {
+
+ 
+    public GameObject explosionPlayer;
+
+    
+
+    // Use this for initialization
+    void Start()
+    {
+       
+    }
+        void OnTriggerEnter2D(Collider2D other)
+        {
+        if(other.tag == "Boundary"||other.tag == "Hazard")
+        {
+            return;
+        }
+
+        if(other.tag == "Player")
+        {
+            Instantiate(explosionPlayer, other.transform.position, other.transform.rotation);
+        }
+
+
+        Destroy(other.gameObject);
+            }
+	
+}
