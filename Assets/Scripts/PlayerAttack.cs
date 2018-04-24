c35e1144f573f4e30977b03bc30cc0e9211249f4
Maserthous <35930761+Maserthous@users.noreply.github.com>
2018-04-24 17:42:06 -0400

    Revert "Revert "Key attack""
    This reverts commit 972b6b71b1230636438d0ff6352cd16b5661f34f.

--- Assets/Scripts/PlayerAttack.cs
+++ Assets/Scripts/PlayerAttack.cs
@@ -5,21 +5,39 @@ using UnityEngine;
 public class PlayerAttack : MonoBehaviour {
 
     public GameObject enemyDeath;
+    public float xForce;
+    public float yForce;
+    public GameObject player;
+
+    private PlayerController pc;
+    private bool key;
+    private bool faceRight;
+
+    private void Start()
+    {
+        pc = player.GetComponent<PlayerController>();
+    }
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.tag == "Boundary" || other.tag == "Hazard" || other.tag == "Platform")
-        {
-            return;
-        }
-
         if (other.tag == "Enemy")
         {
-            Instantiate(enemyDeath, other.transform.position, other.transform.rotation);
+            key = GameController.game.GetKey();
+            faceRight = pc.FaceRight();
+            if (!key)
+            {
+                Instantiate(enemyDeath, other.transform.position, other.transform.rotation);
+                Destroy(other.gameObject);
+            }
+            else if (key && faceRight)
+            {
+                other.GetComponent<Rigidbody2D>().AddForce(new Vector2(xForce, yForce), ForceMode2D.Impulse);
+            }
+            else if (key && !faceRight)
+            {
+                other.GetComponent<Rigidbody2D>().AddForce(new Vector2(-xForce, yForce), ForceMode2D.Impulse);
+            }
         }
-
-
-        Destroy(other.gameObject);
     }
 
 }
